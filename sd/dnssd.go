d3c0fd58f54fdd648208678746eda503850e4a50
zoukai <>
2019-02-24 14:45:46 +0800

    修改dns-sd实现

--- sd/dnssd.go
+++ sd/dnssd.go
@@ -3,45 +3,23 @@ package sd
 import (
 	"context"
 	"fmt"
-	"github.com/brutella/dnssd"
+	"github.com/grandcat/zeroconf"
 )
 
 const serviceName = "mifinder"
 
 func StartSD(ctx context.Context, port int) {
 
-	if resp, err := dnssd.NewResponder(); err != nil {
-		fmt.Println(err)
-	} else {
-		cfg := dnssd.Config{
-			Name: serviceName,
-			Type: "_http._tcp",
-			Port: port,
-		}
-
-		srv, err := dnssd.NewService(cfg)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		handle, err := resp.Add(srv)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		} else {
-			fmt.Printf("service %s: Name now registered and active\n", handle.Service().ServiceInstanceName())
-		}
-
-		ctx, cancel := context.WithCancel(ctx)
-		defer cancel()
-
-		err = resp.Respond(ctx)
-
-		if err != nil {
-			fmt.Println(err)
-		}
+	server, err := zeroconf.Register(serviceName, "_http._tcp", "local.", port, []string{}, nil)
+	if err != nil {
+		panic(err)
 	}
+	defer server.Shutdown()
+
+	fmt.Printf("service %s: Name now registered and active\n", fmt.Sprintf("%s._http._tcp.local.", serviceName))
+
+	select {
+	case <-ctx.Done():
+	}
+	fmt.Sprint("service stop")
 }
--- sd/dnssd_test.go
+++ sd/dnssd_test.go
@@ -1,9 +1,14 @@
 package sd
 
-import "testing"
+import (
+	"context"
+	"testing"
+)
 
 func TestStartSD(t *testing.T) {
-	if err := StartSD(8080); err != nil {
-		t.Error(err)
-	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	StartSD(ctx, 8080)
 }
